acaec2aec75265e0eefa0fbfe7b50220d398de46
Eladamar <eladamar@rishon.csf.technion.ac.il>
2020-05-06 17:14:07 +0300

    changed accuracy to comupte mean

--- metrics.py
+++ metrics.py
@@ -2,7 +2,9 @@ import torch
 
 def pixel_accuracy(preds, label):
     acc_sum = torch.sum(preds.argmax(axis=1) == label).item()
+    # divide by batch size
+    acc_mean = acc_sum / preds.shape[0]
     image_size = preds.shape[2] * preds.shape[3]
-    acc = float(acc_sum) / (image_size + 1e-10)
+    acc = float(acc_mean) / (image_size + 1e-10)
     return acc
 
